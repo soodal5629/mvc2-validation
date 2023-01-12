ce9df1ae5a8a9bde6ac0a1186fff678546dc7181
chaelynjang <chaelynjang@lotte.net>
2023-01-12 20:54:47 +0900

    Bean Validation 에러 메시지 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,16 +2,22 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+/*
+// 아래 주석부분은 제약조건도 많고 그래서 권장하지 않음 => 차라리 자바코드로 작성성라
+@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000"
+                , message = "총합이 10000원 넘게 입력해주세요.")
+*/
 public class Item {
 
     private Long id;
-    @NotBlank
+    @NotBlank(message = "공백X")
     private String itemName;
     @NotNull
     @Range(min = 1000, max = 1000000)
